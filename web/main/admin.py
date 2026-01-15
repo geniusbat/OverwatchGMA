6bd613261793436c30d2567db82711f939c484cb
geniusbat <grandgustaff@gmail.com>
2026-01-15 18:40:33 +0100

    Update on admin page

--- web/main/admin.py
+++ web/main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+admin.register(models.delegate_controls)
+admin.register(models.delegate_errors)
+admin.register(models.hosts_registry)
+admin.register(models.master_controls)
+admin.register(models.master_errors)
