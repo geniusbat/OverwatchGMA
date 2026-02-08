cfc48c4a668560b88ae57718f1e62e63e46adf33
geniusbat <grandgustaff@gmail.com>
2026-02-08 13:52:58 +0100

    Add basic search functionality

--- web/main/views.py
+++ web/main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+import datetime
 
 from . import models
 
@@ -8,22 +9,34 @@ def index(request,test_value=None):
     delegate_controls = models.delegate_controls.objects
     delegate_errors = models.delegate_errors.objects
 
-    host = request.GET.get("host",None)
-    if host != None:
+
+    host = request.GET.get("host","")
+    if host != "":
         delegate_controls = delegate_controls.filter(host=host)
         delegate_errors = delegate_errors.filter(host=host)
-    afterStamp = request.GET.get("afterStamp",None)
-    if afterStamp != None:
+    afterStamp = request.GET.get("afterStamp","")
+    if afterStamp != "":
         delegate_controls = delegate_controls.filter(timestamp__gt=afterStamp)
         delegate_errors = delegate_errors.filter(timestamp__gt=afterStamp)
-    command = request.GET.get("command",None)
-    if command != None:
+    command = request.GET.get("command","")
+    if command != "":
         delegate_controls = delegate_controls.filter(command_name=command)
         delegate_errors = delegate_errors.filter(command_name=command)
-    returncode = request.GET.get("returncode",None)
-    if returncode != None:
+    returncode = request.GET.get("returncode","")
+    if returncode != "":
         delegate_controls = delegate_controls.filter(returncode=returncode)
         delegate_errors = delegate_errors.filter(returncode=returncode)
+
+    now = datetime.datetime.now(datetime.UTC)
+    last_options = [
+        [int((now-datetime.timedelta(minutes=5)).timestamp()), "5 minutes"],
+        [int((now-datetime.timedelta(minutes=30)).timestamp()), "30 minutes"],
+        [int((now-datetime.timedelta(hours=1)).timestamp()), "1 hour"],
+        [int((now-datetime.timedelta(hours=2)).timestamp()), "2 hours"],
+        [int((now-datetime.timedelta(hours=7)).timestamp()), "7 hours"],
+        [int((now-datetime.timedelta(days=1)).timestamp()), "1 day"],
+        [int((now-datetime.timedelta(days=15)).timestamp()), "15 days"]
+    ]
 
     template = "index.html"
     context = {}
@@ -32,5 +45,8 @@ def index(request,test_value=None):
     context["master_controls"] = models.master_controls.objects.all()
     context["delegate_errors"]  = delegate_errors.all()
     context["master_errors"]  = models.master_errors.objects.all()
+    context["hosts"] = models.hosts_registry.objects.all()
+    context["last_options"] = last_options
+
     return render(request, template, context)
 
